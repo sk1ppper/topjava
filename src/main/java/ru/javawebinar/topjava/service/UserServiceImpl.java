fa9e503a5f5debc81905d250daf9cf07700c13fc
sk1ppper <muraturazbaev@icloud.com>
2019-05-15 14:15:33 +0300

    2 7 constructor injection

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -11,7 +11,12 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 public class UserServiceImpl implements UserService {
 
-    private UserRepository repository;
+    private final UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public User create(User user) {
